ae82f0f8ce6be6920f103aa5390edb106825e517
mozict <mozict@gmail.com>
2025-06-01 17:30:19 +0700

    awalan

---
+++ components/Header/Test.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="line-b fixed w-full top-0 z-20 flex h-14 items-center justify-between bg-white px-4 after:-bottom-px sm:px-6">
+        <div class="grow">
+            <p>Hi, welcome back test !</p>
+        </div>
+    </header>
+</template>
---
+++ layouts/admin.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid items-start lg:flex">
+    <Sidebar />
+    <div class="w-full p-8">
+      <HeaderTest />
+      <slot />
+    </div>
+  </div>
+</template>
